69d1b531cede3f1dd08207340abd5a0f4c2a5f7d
dsicari <danilo@dsicari.com.br>
2020-07-09 18:54:27 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+//import 'package:mandatrampo/screens/homepage.dart';
+import 'package:mandatrampo/screens/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    var borderRadius2 = BorderRadius.circular(10);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        //Altera as cores dos componentes para o Tema Escuro
+        brightness: Brightness.dark,
+        accentColor: Color(0xFF746EFF),
+        // Cor de fundo do Scaffold
+        scaffoldBackgroundColor: Color(0xFF171717),      
+        // Cor padrão dos botões
+        buttonTheme: ButtonThemeData(
+          buttonColor: Color(0xFF746EFF),
+        ),
+        // Decoração das caixas de texto com bordas arredondadas
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Color(0xFF7D7B85),
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Color(0xFF7D7B85),
+            ),
+            borderRadius: borderRadius2,
+          ),
+        ),
+      ),
+      home: LoginPage(),
+    );
+  }
+}
